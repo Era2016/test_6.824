a1cb501a4be67298e60567dd136df7c357966a68
zhangjiyuan <zhangjiyuan1@joyme.sg>
2023-02-24 19:40:39 +0800

    add log struct

--- src/raft/util.go
+++ src/raft/util.go
@@ -1,13 +1,18 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
 // Debugging
 const Debug = false
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug {
-		log.Printf(format, a...)
+		logger := log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds)
+		logger.Printf(format, a...)
+		//log.Printf(format, a...)
 	}
 	return
 }
